82bb1b323acb34c2b8dc6a71f702bd6a8ad65c11
goncalo-machado <goncalofmachado01@gmail.com>
2024-10-05 19:02:48 +0100

    favorite works, doing tabs now

--- homework/lib/main.dart
+++ homework/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 import 'dart:convert';
 import 'models.dart';
-
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -15,16 +16,36 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pokedex Demo',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 60, 62, 209)),
-        fontFamily: 'PokemonClassic',
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Pokedex Demo',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 60, 62, 209)),
+          fontFamily: 'PokemonClassic',
+        ),
+        home: HomePage(),
       ),
-      home: HomePage(),
     );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var favorites = <PokemonBase>[];
+
+  void toggleFavorite(PokemonBase pokemon) {
+    if (isFavorite(pokemon)) {
+      favorites.remove(pokemon);
+    } else {
+      favorites.add(pokemon);
+    }
+    notifyListeners();
+  }
+
+  bool isFavorite(PokemonBase pokemon) {
+    return favorites.contains(pokemon);
   }
 }
 
@@ -38,6 +59,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<PokemonBase> pokemons = [];
   bool isLoading = true;
+  bool detailsClicked = false;
 
   @override
   void initState() {
@@ -63,15 +85,19 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  Future<void> _pokemonDetails(BuildContext context, String url) async {
-    var response = await http.get(Uri.parse(url));
+  Future<void> _pokemonDetails(
+      BuildContext context, PokemonBase pokemon) async {
+    var response = await http.get(Uri.parse(pokemon.url));
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
+      final details = PokemonDetails.fromJson(data);
       if (context.mounted) {
+        detailsClicked = false;
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => DetailsPage(data: data),
+            builder: (context) =>
+                DetailsPage(details: details, pokemon: pokemon),
           ),
         );
         // Navigator.push(
@@ -94,8 +120,6 @@ class _HomePageState extends State<HomePage> {
         //           );
         //         }));
       }
-    } else {
-      // Handle error
     }
   }
 
@@ -107,80 +131,188 @@ class _HomePageState extends State<HomePage> {
             style: TextStyle(fontFamily: 'PokemonClassic')),
         backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            SizedBox(
-              height: 100,
-              child: DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                ),
-                child: Text('Menu'),
-              ),
-            ),
-            ListTile(
-              title: const Text('Pokedex'),
-              onTap: () {
-                print("Item 1");
-              },
-            ),
-            ListTile(
-              title: const Text('Team'),
-              onTap: () {
-                print("Item 2");
-              },
-            ),
-            ListTile(
-              title: const Text('Favorites'),
-              onTap: () {
-                print("Item 2");
-              },
-            ),
-          ],
-        ),
-      ),
+      // drawer: Drawer(
+      //   child: ListView(
+      //     padding: EdgeInsets.zero,
+      //     children: [
+      //       SizedBox(
+      //         height: 100,
+      //         child: DrawerHeader(
+      //           decoration: BoxDecoration(
+      //             color: Theme.of(context).colorScheme.primaryContainer,
+      //           ),
+      //           child: Text('Menu'),
+      //         ),
+      //       ),
+      //       ListTile(
+      //         title: const Text('Pokedex'),
+      //         onTap: () {
+      //           print("Item 1");
+      //         },
+      //       ),
+      //       ListTile(
+      //         title: const Text('Team'),
+      //         onTap: () {
+      //           print("Item 2");
+      //         },
+      //       ),
+      //       ListTile(
+      //         title: const Text('Favorites'),
+      //         onTap: () {
+      //           print("Item 2");
+      //         },
+      //       ),
+      //     ],
+      //   ),
+      // ),
       body: isLoading
           ? Center(child: CircularProgressIndicator())
-          : ListView(
-              children: [
-                for (var pokemon in pokemons)
-                  ListTile(
-                    title: Text(pokemon.name),
-                    leading: Text("#${pokemon.number}"),
-                    onTap: () => _pokemonDetails(context, pokemon.url),
-                  )
-              ],
+          : Consumer<MyAppState>(
+              builder: (context, value, child) {
+                return ListView.builder(
+                  itemCount: pokemons.length,
+                  itemBuilder: (context, index) {
+                    final pokemon = pokemons[index];
+                    final icon = value.isFavorite(pokemon)
+                        ? Icons.favorite
+                        : Icons.favorite_border;
+                    return ListTile(
+                      title: Text(pokemon.name),
+                      leading: Text("#${pokemon.number}"),
+                      onTap: () => {
+                        if (!detailsClicked)
+                          {_pokemonDetails(context, pokemon)},
+                        detailsClicked = true,
+                      },
+                      trailing: IconButton(
+                          onPressed: () {
+                            value.toggleFavorite(pokemon);
+                          },
+                          icon: Icon(icon)),
+                    );
+                  },
+                );
+              },
             ),
     );
   }
 }
 
 class DetailsPage extends StatelessWidget {
-  final dynamic data;
+  final PokemonDetails details;
+  final PokemonBase pokemon;
 
   const DetailsPage({
-    required this.data,
+    required this.details,
+    required this.pokemon,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    print(data);
+    print(details);
+    // for (var type in details.types) {
+    //   Color? color = typeColors["Fire"];
+    //   Color? color2 = typeColors[type];
+    //   print(color.toString());
+    //   print(color2.toString());
+    // }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Route'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Data: $data!'),
+    return Consumer<MyAppState>(builder: (context, value, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('#${details.number} ${details.name}'),
+          actions: [
+            IconButton(
+                icon: Icon(value.isFavorite(pokemon)
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                onPressed: () {
+                  value.toggleFavorite(pokemon);
+                }),
+          ],
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                  height: 260.0,
+                  width: 260.0,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      image: DecorationImage(
+                        image: NetworkImage(details.image, scale: 0.2),
+                        fit: BoxFit.cover,
+                      ))),
+              const Divider(
+                thickness: 1,
+                endIndent: 0,
+                color: Colors.grey,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Height ${details.height} mt",
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 12.0, // Set the desired font size
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.grey),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            "Weight ${details.weight} Kg",
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 12.0, // Set the desired font size
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 10, 0, 0),
+                child: Row(
+                  children: [
+                    Text("Types:"),
+                    for (var type in details.types)
+                      Chip(
+                        label: Text(type),
+                        backgroundColor: typeColors[type],
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        visualDensity: const VisualDensity(
+                            horizontal: 0, vertical: -4), // Add this line
+                      ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    });
   }
 }
--- homework/lib/models.dart
+++ homework/lib/models.dart
@@ -1,3 +1,29 @@
+import 'package:flutter/material.dart';
+
+Map<String, Color> typeColors = {
+  "Normal": Color.fromRGBO(168, 168, 120, 1.0),
+  "Fighting": Color.fromRGBO(192, 48, 40, 1.0),
+  "Flying": Color.fromRGBO(168, 144, 240, 1.0),
+  "Poison": Color.fromRGBO(160, 64, 160, 1.0),
+  "Ground": Color.fromRGBO(224, 192, 104, 1.0),
+  "Rock": Color.fromRGBO(184, 160, 56, 1.0),
+  "Bug": Color.fromRGBO(168, 184, 32, 1.0),
+  "Ghost": Color.fromRGBO(112, 88, 152, 1.0),
+  "Steel": Color.fromRGBO(184, 184, 208, 1.0),
+  "Fire": Color.fromRGBO(240, 128, 48, 1.0),
+  "Water": Color.fromRGBO(104, 144, 240, 1.0),
+  "Grass": Color.fromRGBO(120, 200, 80, 1.0),
+  "Electric": Color.fromRGBO(248, 208, 48, 1.0),
+  "Psychic": Color.fromRGBO(248, 88, 136, 1.0),
+  "Ice": Color.fromRGBO(152, 216, 216, 1.0),
+  "Dragon": Color.fromRGBO(112, 56, 248, 1.0),
+  "Dark": Color.fromRGBO(112, 88, 72, 1.0),
+  "Fairy": Color.fromRGBO(238, 153, 172, 1.0),
+  "Stellar": Color.fromRGBO(190, 140, 255, 1.0),
+  "Unknown": Color.fromRGBO(104, 160, 144, 1.0),
+  "Shadow": Color.fromRGBO(100, 78, 136, 1.0),
+};
+
 class PokemonBase {
   final String name;
   final String url;
@@ -14,6 +40,57 @@ class PokemonBase {
     String url = json['url'];
     int number = int.parse(url.split('/')[6]);
 
-    return PokemonBase(name: name[0].toUpperCase() + name.substring(1), url: url, number: number);
+    return PokemonBase(
+        name: name[0].toUpperCase() + name.substring(1),
+        url: url,
+        number: number);
+  }
+}
+
+class PokemonDetails {
+  final String name;
+  final int number;
+
+  final String image;
+  final double weight;
+  final double height;
+
+  final List<String> types;
+
+  const PokemonDetails({
+    required this.name,
+    required this.number,
+    required this.image,
+    required this.height,
+    required this.weight,
+    required this.types,
+  });
+
+  factory PokemonDetails.fromJson(Map<String, dynamic> json) {
+    String name = json['name'];
+    int number = json['id'];
+    String image = json['sprites']['front_default'];
+    final double weight = json['weight']/10;
+    final double height = json['height']/10;
+
+    List<dynamic> jsonTypes = json['types'];
+    List<String> types = [];
+
+    for (var type in jsonTypes) {
+      types.add(type['type']['name'][0].toUpperCase() + type['type']['name'].substring(1));
+    }
+
+    return PokemonDetails(
+        name: name[0].toUpperCase() + name.substring(1),
+        number: number,
+        image: image,
+        weight: weight,
+        height: height,
+        types: types);
+  }
+
+  @override
+  String toString() {
+    return "{ Name : $name, Number : $number, Image : $image, Weight : $weight, Height : $height, Types : $types";
   }
 }
